e49b119e3fc2427b8e751adcbc2ad6b59b3afd3e
Wesley Crosslin <136374861+Weskc12@users.noreply.github.com>
2023-08-09 11:29:55 +0000

    domain generator with bonus objective

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".us", ".net", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          console.log(pronoun + adj + noun + domain);
+        }
+      }
+    }
+  };
